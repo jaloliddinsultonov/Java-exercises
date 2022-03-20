440c4b3bc2d1f15c2ddc3bc9b335d1590e9573f4
Jaloliddin-Sultonov <94798415+Jaloliddin-Sultonov@users.noreply.github.com>
2022-03-20 18:58:29 +0100

    lab 4 exercises

---
+++ 4-lab/Car.java
@@ -0,0 +1,91 @@
+
+public class Car {
+    private String model;
+    private String brand;
+    private int year;
+    private int price;
+    private String color;
+    private int quantity;
+
+    public Car(String model, String brand, int year, int price, String color, int quantity) {
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.quantity = quantity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", brand='" + brand + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
+
+
+public class CarDemo {
+    public static void main(String[] args) {
+
+        Car car1 = new Car ("GLE", "Mersedes", 2020, 350000, "white", 5);
+        System.out.println(car1.toString());
+        System.out.println(car1.getColor());
+        int year = car1.getYear();
+        car1.setColor("black");
+        System.out.println(car1.toString());
+    }
+}
---
+++ 4-lab/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        RectangleArea rg = new RectangleArea();
+        rg.getData();
+        rg.computeField();
+        rg.fieldDisplay();
+    }
+}
